143a3981916b71fbf25ec81caa3d29a3c54d6864
akshayahuja15 <83705640+akshayahuja15@users.noreply.github.com>
2022-05-16 01:31:03 +0530

    Add files via upload

---
+++ wp-content/themes/petie/archive-osf_service.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+    <div class="wrap">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main">
+                <?php if (have_posts()) :
+                    echo '<div class="row" data-elementor-columns="3">';
+                    while (have_posts()) : the_post();
+                        get_template_part('template-parts/service/content', 'service');
+                    endwhile;
+                    echo '</div>';
+                    the_posts_pagination(array(
+                        'prev_text'          => '<span class="arrow">&larr;</span><span class="screen-reader-text">' . esc_html__( 'Previous', 'petie' ) . '</span>',
+                        'next_text'          => '<span class="screen-reader-text">' . esc_html__( 'Next', 'petie' ) . '</span><span class="arrow">&rarr;</span>',
+                        'before_page_number' => '<span class="meta-nav screen-reader-text">' . esc_html__( 'Page', 'petie' ) . ' </span>',
+                    ));
+                else :
+                    get_template_part('template-parts/post/content', 'none');
+                endif; ?>
+            </main><!-- #main -->
+        </div><!-- #primary -->
+        <?php get_sidebar(); ?>
+    </div><!-- .wrap -->
+<?php get_footer();
