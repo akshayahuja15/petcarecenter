ed140ffcb02b23d15bed1c9d13e529d77fab3743
akshayahuja15 <83705640+akshayahuja15@users.noreply.github.com>
2022-05-16 01:48:23 +0530

    Delete wp-content directory

--- wp-content/themes/petie/template-parts/footer/builder.php
+++
@@ -1,5 +0,0 @@
-<div class="wrap">
-    <div class="container">
-        <?php petie_render_footer(); ?>
-    </div>
-</div>
--- wp-content/themes/petie/template-parts/service/content-single.php
+++
@@ -1,7 +0,0 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class('osf-service-single'); ?>>
-    <div class="post-inner">
-        <div class="entry-content">
-            <?php the_content(); ?>
-        </div><!-- .entry-content -->
-    </div>
-</article><!-- #post-## -->
